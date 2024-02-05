93ec24f97c1ceb09ef58d91ffeeb6226bcefb9cd
RonexLemon <ronexondimu@gmail.com>
2024-02-05 08:29:21 +0300

    save user

--- models/user.go
+++ models/user.go
@@ -1,6 +1,12 @@
 package models
 
 import (
+	"ginrestapi/database"
+	"html"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
+
 	"gorm.io/gorm"
 )
 
@@ -9,4 +15,24 @@ type User struct {
 	Username string `gorm:"size:255;not null;unique" json:"username"`
     Password string `gorm:"size:255;not null;" json:"password"`
 	Entry    []Book
+}
+
+
+func( user *User) Save() (*User,error){
+	err:= database.Database.Create(&user).Error
+	if err !=nil{
+		return &User{}, err
+	}
+	return user,nil
+}
+
+func (user *User) BeforeSave(*gorm.DB)error{
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.DefaultCost)
+
+	if err !=nil{
+		return err;
+	}
+	user.Password = string(passwordHash)
+	user.Username = html.EscapeString((strings.TrimSpace(user.Username)))
+	return nil;
 }
