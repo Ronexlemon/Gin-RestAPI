c80230e64946892b6b89934855daf5686880327c
RonexLemon <ronexondimu@gmail.com>
2024-02-05 07:30:34 +0300

    add user model

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 router:= gin.Default()
+router.GET("/books",func(c *gin.Context){
+	c.JSON(http.StatusFound, gin.H{"data":"hello"})
+})
+
+router.Run(":8000")
 
 }
