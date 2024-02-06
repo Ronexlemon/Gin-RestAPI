9af1eca92fa4a237a6d218bf71c35d5b9cfd9d85
RonexLemon <ronexondimu@gmail.com>
2024-02-06 05:30:19 +0300

    login user

--- controller/authentication.go
+++ controller/authentication.go
@@ -1,31 +1,57 @@
 package controller
 
 import (
-	
+	"ginrestapi/helper"
 	"ginrestapi/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Register(context *gin.Context){
+func Register(context *gin.Context) {
 	var input models.AuthenticationInput
-	if err:= context.ShouldBindJSON(&input); err!= nil{
-		context.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	user := models.User{
 		Username: input.Username,
 		Password: input.Password,
 	}
-	savedUser, err:= user.Save()
+	savedUser, err := user.Save()
 
-	if err!= nil{
-		context.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	context.JSON(http.StatusCreated,gin.H{"user":savedUser})
-	
+	context.JSON(http.StatusCreated, gin.H{"user": savedUser})
+
 }
 
+func Login(context *gin.Context) {
+	var inputs models.AuthenticationInput
 
+	if err := context.ShouldBindJSON(&inputs); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	user, err := models.FindUserByUsername(inputs.Username)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err = user.ValidatePassowrd(inputs.Password)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	jwt, err := helper.GenerateJWT(user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"jwt": jwt})
+
+}
