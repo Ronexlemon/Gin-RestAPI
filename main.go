3cbec14ee6715e0e3b210a14594a377c9a914799
RonexLemon <ronexondimu@gmail.com>
2024-02-06 05:50:06 +0300

    rest api

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"ginrestapi/controller"
 	"ginrestapi/database"
+	"ginrestapi/middleware"
 	"ginrestapi/models"
 	"log"
 
@@ -41,6 +42,12 @@ func serverApplication(){
 	publicRoutes := router.Group("/auth")
 	publicRoutes.POST("/register",controller.Register)
 	publicRoutes.POST("/login",controller.Login)
+
+
+	protectedRoutes := router.Group("/api")
+    protectedRoutes.Use(middleware.JWTAuthMiddleware())
+    protectedRoutes.POST("/entry", controller.GetAllEntries)
+    protectedRoutes.GET("/entry", controller.GetAllEntries)
 
 	router.Run(":8000")
 	fmt.Println("Surver running on port 8000")
