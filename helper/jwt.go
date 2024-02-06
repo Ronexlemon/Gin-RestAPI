483e1edccb8c7230dc3296a8d74a818b66c1a275
RonexLemon <ronexondimu@gmail.com>
2024-02-06 05:22:46 +0300

    jwt token

--- helper/jwt.go
+++ helper/jwt.go
@@ -1,2 +1,24 @@
 // create jwt tokens
 package helper
+
+import (
+	"ginrestapi/models"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var privateKey =[]byte(os.Getenv("JWT_PRIVATE_KEY"))
+
+func GenerateJWT(user models.User) (string,error){
+	tokenTTL,_:= strconv.Atoi(os.Getenv("TOKEN_TTL"))
+	token:= jwt.NewWithClaims(jwt.SigningMethodES256,jwt.MapClaims{
+		"id" :user.ID,
+		"iat" :time.Now().Unix(),
+		"eat" : time.Now().Add(time.Second * time.Duration(tokenTTL)).Unix(),
+
+	})
+	return token.SignedString(privateKey)
+}
